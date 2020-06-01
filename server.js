6cfbf51b6c33195dbd7e3bed2ea8e49483a83497
jaypal solanki <jaypsl263@gmail.com>
2020-06-01 13:20:07 -0400

    setting up backend for login/register page

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./models/validation/routes/api/users");
 
 const app = express();
 
-//middleware for bodyparser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// db config
 const db = require("./config/keys.js").mongoURI;
 
-//connect to mongoose
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("mongodb is connected"))
   .catch((err) => console.log(err));
 
-//create a port
+app.use(passport.initialize());
+require("./config/passport")(passport);
+app.use("/api/users", users);
+
 const port = process.env.port || 5000;
 
 app.listen(port, () => console.log(`server is running at ${port}`));
