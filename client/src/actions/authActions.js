4de35c1610db18fa35d90e3f1c63ee447c1e2cba
jaypal solanki <jaypsl263@gmail.com>
2020-06-22 13:45:22 -0400

    proxy not working

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -5,7 +5,7 @@ import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
 
 export const registerUser = (userData, history) => (dispatch) => {
   axios
-    .post("/api/users/register", userData)
+    .post("http://localhost:5000/api/users/register", userData)
     .then((res) => history.push("/login"))
     .catch((err) =>
       dispatch({
@@ -17,7 +17,7 @@ export const registerUser = (userData, history) => (dispatch) => {
 
 export const loginUser = (userData) => (dispatch) => {
   axios
-    .post("/api/users/login", userData)
+    .post("http://localhost:5000/api/users/login", userData)
     .then((res) => {
       const { token } = res.data;
       localStorage.setItem("jwtToken", token);
