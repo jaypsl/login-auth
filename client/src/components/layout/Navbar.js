33b3b4d3a275ce7cd6fd7918f007f203d04d2c70
jaypal solanki <jaypsl263@gmail.com>
2020-06-22 13:47:37 -0400

    remove logo

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,18 +5,7 @@ class Navbar extends Component {
     return (
       <div className="navbar-fixed">
         <nav className="z-depth-0">
-          <div className="nav-wrapper white">
-            <Link
-              to="/"
-              style={{
-                fontFamily: "monospace",
-              }}
-              className="col s5 brand-logo center black-text"
-            >
-              <i className="material-icons">code</i>
-              MERN
-            </Link>
-          </div>
+          <div className="nav-wrapper white"></div>
         </nav>
       </div>
     );
